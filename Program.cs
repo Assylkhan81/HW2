9591772746a57cbc0fa55ee34b1c9061ba278c88
Assylkhan81 <asylkhan@list.ru>
2022-09-02 14:51:45 +0600

    По двум заданным числам проверять является ли первое квадратом второго

--- Program.cs
+++ Program.cs
@@ -23,3 +23,18 @@ double Decision(double x1, double x2,
   }
 double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
 Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+// По двум заданным числам проверять является ли первое квадратом второго 
+// Данную задчу решали в прошлый раз - тут функция вроде и не нужна.
+//  Console.WriteLine("Введите 1-ое число ");
+    // int numberA = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите 2-ое число ");
+    // int numberB = Convert.ToInt32(Console.ReadLine());
+    // if (numberA == numberB * numberB)
+    //     {
+    //         Console.WriteLine("Da");
+    //     }
+    // else 
+    //     {
+    //         Console.WriteLine("net");
+    //     }
